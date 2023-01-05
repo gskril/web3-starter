defeda32a657905f465b022e5f6aa599a852bc35
Greg <gskril@gmail.com>
2023-01-05 22:28:56 +0200

    Add Plausible analytics

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const { withPlausibleProxy } = require('next-plausible')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPlausibleProxy()(nextConfig)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.scss'
+import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="example.com" trackOutboundLinks>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
