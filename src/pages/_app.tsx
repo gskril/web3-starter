77e4505a0227507282c3d3d9f7b0eaa8a9e9071c
Greg <gskril@gmail.com>
2023-01-05 23:15:24 +0200

    Add web3 libs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,40 @@
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
 import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { mainnet, goerli } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 
 import '../styles/globals.scss'
+
+const ALCHEMY_ID = process.env.ALCHEMY_ID || ''
+
+const { chains, provider } = configureChains(
+  [mainnet, goerli],
+  [alchemyProvider({ apiKey: ALCHEMY_ID }), publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Web3 Starter',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PlausibleProvider domain="example.com" trackOutboundLinks>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} modalSize="compact">
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </PlausibleProvider>
   )
 }
