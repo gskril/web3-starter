756ffc4b26cc2f428d700680a49e155281a90a21
Greg <gskril@gmail.com>
2023-01-13 20:39:49 +0200

    Clean up web3 providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,11 @@
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
 import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { mainnet, goerli } from 'wagmi/chains'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public'
+import { WagmiConfig } from 'wagmi'
 
+import { chains, wagmiClient } from '../providers'
 import '../styles/style.scss'
-
-const ALCHEMY_ID = process.env.ALCHEMY_ID || ''
-
-const { chains, provider } = configureChains(
-  [mainnet, goerli],
-  [alchemyProvider({ apiKey: ALCHEMY_ID }), publicProvider()]
-)
-
-const { connectors } = getDefaultWallets({
-  appName: 'Web3 Starter',
-  chains,
-})
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
---
+++ src/providers.ts
@@ -0,0 +1,25 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { mainnet, goerli } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+
+const ALCHEMY_ID = process.env.NEXT_PUBLIC_ALCHEMY_ID || ''
+
+export const { chains, provider } = configureChains(
+  [mainnet, goerli],
+  [alchemyProvider({ apiKey: ALCHEMY_ID }), publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Web3 Starter',
+  chains,
+})
+
+export const wagmiClientConfig = {
+  autoConnect: true,
+  connectors,
+  provider,
+}
+
+export const wagmiClient = createClient(wagmiClientConfig)
