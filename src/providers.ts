f033f5f02da945e72ef85291b6a2066311688d04
Greg <gskril@gmail.com>
2023-02-03 13:57:34 +0200

    Upgrade deps

--- src/providers.ts
+++ src/providers.ts
@@ -1,5 +1,5 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { configureChains, createClient } from 'wagmi'
 import { mainnet, goerli } from 'wagmi/chains'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
