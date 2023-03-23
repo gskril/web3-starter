1531c4874adf04ddecc8010201ea000e332da5f5
Greg <gskril@gmail.com>
2023-03-22 20:56:51 -0400

    Enable styled-components SWC transform

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const { withPlausibleProxy } = require('next-plausible')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
 }
 
 module.exports = withPlausibleProxy()(nextConfig)
