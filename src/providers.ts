2a19a9bcd8d58ecdf7e538a6d7d4a73dee9f3a47
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-06-29 23:18:39 -0400

    Update to WalletConnect v2

--- src/providers.ts
+++ src/providers.ts
@@ -1,19 +1,23 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createConfig } from 'wagmi'
 import { goerli, mainnet } from 'wagmi/chains'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 
-const ALCHEMY_ID = process.env.NEXT_PUBLIC_ALCHEMY_ID || ''
+const WALLETCONNECT_ID = process.env.NEXT_PUBLIC_WALLETCONNECT_ID
+
+if (!WALLETCONNECT_ID) {
+  throw new Error('Missing NEXT_PUBLIC_WALLETCONNECT_ID')
+}
+
 export const chains = [mainnet, goerli]
 
 const { publicClient, webSocketPublicClient } = configureChains(chains, [
-  alchemyProvider({ apiKey: ALCHEMY_ID }),
   publicProvider(),
 ])
 
 const { connectors } = getDefaultWallets({
   appName: 'Web3 Starter',
+  projectId: WALLETCONNECT_ID,
   chains,
 })
 
